b9fdac157e5cbd46ce7ce32ccaedf986ef3da344
salbarra <98361072+salbarra@users.noreply.github.com>
2024-04-19 13:47:52 -0500

    Primer cargue

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Home } from "@mui/icons-material"
+import { ThemeProvider } from "@mui/material"
+
+
+const App = () => {
+   
+return (
+<ThemeProvider theme={theme}>
+  <Home />
+</ThemeProvider>
+
+)
+
+
+}
+export default App
---
+++ src/page/Home.tsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/material"
+import CrButton from "../components/CrButton"
+import CrCard from "../components/CrCard"
+import CrNavBar from "../components/CrNavBar"
+
+const Home = () => {
+return(
+    <>
+        <CrNavBar />
+        <Box></Box>
+        <CrCard />
+        <CrButton/>
+    </>
+    )
+}
+export default Home
